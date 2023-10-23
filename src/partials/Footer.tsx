c0074a3d5b841b26d575f8a94f96eecd5bc0d513
NeeKin <njgzs360@gmail.com>
2023-10-23 18:36:07 +0800

    add 备案

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -2,7 +2,7 @@ import { FooterCopyright, Section } from 'astro-boilerplate-components';
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={'Nee Kin'} />
+    <FooterCopyright site_name={'Nee Kin'} site_beian={'粤ICP备16017494号-2'} />
   </Section>
 );
 
